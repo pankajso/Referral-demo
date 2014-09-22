59b1ac4215170decfcd405575cdd8237d90c5929
Pankaj <pankajso@gmail.com>
2014-09-22 19:12:17 +0530

    assets added, app layout haml added, routes modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_filter :capture_referal
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :alert => exception.message
+  end
+
+  private
+  def capture_referal
+    session[:referral] = params[:referral] if params[:referral]
+  end
 end
